d9b6ba039564e29436475998667825b7ee8eeca5
Gloria Palm <gloria.palm7810@gmail.com>
2023-09-21 08:12:17 +0200

    Added database, components and scr

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,18 +1,46 @@
-const sqlite3 = require('sqlite3').verbose();
-const { open } = require('sqlite');
+// const sqlite3 = require('sqlite3').verbose();
+// const { open } = require('sqlite');
+
+// const database = {
+//     openDb: async function openDb() {
+//         let dbFilename = `./db/trains.sqlite`;
+
+//         if (process.env.NODE_ENV === 'test') {
+//             dbFilename = "./db/test.sqlite";
+//         }
+
+//         return await open({
+//             filename: dbFilename,
+//             driver: sqlite3.Database
+//         });
+//     }
+// };
+
+// module.exports = database;
+
+const mongo = require("mongodb").MongoClient;
+const collectionName = "keys";
 
 const database = {
-    openDb: async function openDb() {
-        let dbFilename = `./db/trains.sqlite`;
+    getDb: async function getDb() {
+        let dsn = `mongodb+srv://gloriapalm7810:<password>@cluster0.v8m4hqa.mongodb.net/?retryWrites=true&w=majority`;
 
         if (process.env.NODE_ENV === 'test') {
-            dbFilename = "./db/test.sqlite";
+            dsn = "mongodb://localhost:27017/test";
         }
 
-        return await open({
-            filename: dbFilename,
-            driver: sqlite3.Database
+        const client  = await mongo.connect(dsn, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         });
+        const db = await client.db();
+        const collection = await db.collection(collectionName);
+
+        return {
+            db: db,
+            collection: collection,
+            client: client,
+        };
     }
 };
 
