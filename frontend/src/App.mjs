3ee467894e24476c4813bf79f8db54a78e78986e
Gloria Palm <gloria.palm7810@gmail.com>
2023-10-19 14:44:56 +0200

    Fix App.mjs

--- frontend/src/App.mjs
+++ frontend/src/App.mjs
@@ -2,10 +2,10 @@ import React  from 'react';
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import HomePage from "./pages/HomePage.mjs";
-import MapPage from "./pages/MapPage.mjs";
-import StationPage from "./pages/StationPage.mjs";
-import TrainMessagePage from "./pages/TrainMessagePage.mjs";
-import TrainPage from "./pages/TrainPage.mjs";
+// import MapPage from "./pages/MapPage.mjs";
+// import StationPage from "./pages/StationPage.mjs";
+// import TrainMessagePage from "./pages/TrainMessagePage.mjs";
+// import TrainPage from "./pages/TrainPage.mjs";
 // import reportWebVitals from './reportWebVitals.mjs';
 import DelayedTrainPage from "./pages/DelayedTrainPage.mjs";
 
@@ -16,18 +16,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<DelayedTrainPage />}>
-          </Route>
-          <Route path="/train/:trainIdent/:searchDate?" element={<TrainPage />}>
-          </Route>
-          <Route path="/station/:locationId/:limit?" element={<StationPage />}>
-          </Route>
-          <Route path="/arrivals/:locationId/:limit?" element={<StationPage type="arrivals" />}>
-          </Route>
-          <Route path="/departures/:locationId/:limit?" element={<StationPage type="departures" />}>
-          </Route>
-          <Route path="/msg/:locationId?" element={<TrainMessagePage />}>
-          </Route>
-          <Route path="/map/:trainIdent/:searchDate?" element={<MapPage />}>
           </Route>
         </Routes>
       </Router>
