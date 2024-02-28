19d2147fa3031f0ca389b2d69fc8ed659c33cca5
hafsaChy <117537827+hafsaChy@users.noreply.github.com>
2024-02-28 17:50:32 +0100

    Update tickets.js

--- backend/models/tickets.js
+++ backend/models/tickets.js
@@ -1,58 +1,46 @@
 import database from '../db/database.js';
 import { ObjectId } from 'mongodb';
-const collectionName = "tickets";
-let dbName = "trains";
 
-if (process.env.NODE_ENV === 'test') {
-    dbName = "test";
-}
-
+/**
+ * Object for working with collection tickets in database
+ */
 const tickets = {
-    getTickets: async function getTickets(req, res) {
-        // Access a MongoClient object
-        const db = await database.openDb();
+    collectionName: "tickets",
 
-        // Get the collection object
-        const collection = db.collection(collectionName);
+    getTickets: async function getTickets() {
+        const allTickets = await database.getCollection(tickets.collectionName);
 
-        // Get all documents (tickets) in the collection (trains)
-        let allTickets = await collection.find().toArray();
-
-        // Close the database connection
-        await db.client.close();
-
-        // Print all documents to the console
-        console.log(allTickets);
-        return res.json({
-            data: allTickets
-        });
+        return allTickets;
     },
 
-    createTicket: async function createTicket(req, res) {
-        // Access a MongoClient object
+    createTicket: async function createTicket(args) {
         const db = await database.openDb();
+        const collection = await db.collection(tickets.collectionName);
+        // Create a new ObjectId for the new document
+        const newId = new ObjectId();
 
-        // Get the collection object
-        const collection = db.collection(collectionName);
+        const result = await collection.insertOne({
+            _id: newId,
+            code: args.code,
+            trainnumber: args.trainnumber,
+            traindate: args.traindate
+        });
 
-        // Create new document (ticket) in the collection (trains)
-        let newTicket = req.body;
-
-        await collection.insertOne(newTicket);
-
-        // Close the database connection
         await db.client.close();
 
-        // Print all documents to the console
-        console.log(newTicket);
-        return res.json({
-            data: newTicket
-        });
+        // Here we return the string of the ObjectId but alternatives are available
+        // https://www.mongodb.com/docs/manual/reference/method/ObjectId/#ObjectId
+        return {
+            _id: newId.toString(),
+            code: args.code,
+            trainnumber: args.trainnumber,
+            traindate: args.traindate,
+        };
     },
 
     updateTicket: async function updateTicket(args) {
         const db = await database.openDb();
-        const collection = await db.collection(collectionName);
+        const collection = await db.collection(tickets.collectionName);
         // Create ObjectId based on given _id string
         const ticketId = new ObjectId(args._id);
 
@@ -73,7 +61,7 @@ const tickets = {
 
     deleteTicket: async function deleteTicket(args) {
         const db = await database.openDb();
-        const collection = await db.collection(collectionName);
+        const collection = await db.collection(tickets.collectionName);
         // Create ObjectId based on given _id string
         const ticketId = new ObjectId(args._id);
 
@@ -86,9 +74,106 @@ const tickets = {
         if ( result.deletedCount > 0 ) {
             return {
                 _id: args._id
-            };
+            }
         }
     }
 };
 
-export default tickets;
+module.exports = tickets;
+
+
+
+// import database from '../db/database.js';
+// import { ObjectId } from 'mongodb';
+// const collectionName = "tickets";
+// let dbName = "trains";
+
+// if (process.env.NODE_ENV === 'test') {
+//     dbName = "test";
+// }
+
+// const tickets = {
+//     getTickets: async function getTickets(req, res) {
+//         // Access a MongoClient object
+//         const db = await database.openDb();
+
+//         // Get the collection object
+//         const collection = db.collection(collectionName);
+
+//         // Get all documents (tickets) in the collection (trains)
+//         let allTickets = await collection.find().toArray();
+
+//         // Close the database connection
+//         await db.client.close();
+
+//         // Print all documents to the console
+//         console.log(allTickets);
+//         return res.json({
+//             data: allTickets
+//         });
+//     },
+
+//     createTicket: async function createTicket(req, res) {
+//         // Access a MongoClient object
+//         const db = await database.openDb();
+
+//         // Get the collection object
+//         const collection = db.collection(collectionName);
+
+//         // Create new document (ticket) in the collection (trains)
+//         let newTicket = req.body;
+
+//         await collection.insertOne(newTicket);
+
+//         // Close the database connection
+//         await db.client.close();
+
+//         // Print all documents to the console
+//         console.log(newTicket);
+//         return res.json({
+//             data: newTicket
+//         });
+//     },
+
+//     updateTicket: async function updateTicket(args) {
+//         const db = await database.openDb();
+//         const collection = await db.collection(collectionName);
+//         // Create ObjectId based on given _id string
+//         const ticketId = new ObjectId(args._id);
+
+//         const result = await collection.updateOne(
+//             { _id: ticketId },
+//             { $set: { code: args.code } }
+//         );
+
+//         await db.client.close();
+
+//         if ( result.modifiedCount > 0 ) {
+//             return {
+//                 _id: args._id,
+//                 code: args.code
+//             };
+//         }
+//     },
+
+//     deleteTicket: async function deleteTicket(args) {
+//         const db = await database.openDb();
+//         const collection = await db.collection(collectionName);
+//         // Create ObjectId based on given _id string
+//         const ticketId = new ObjectId(args._id);
+
+//         const result = await collection.deleteOne(
+//             { _id: ticketId }
+//         );
+
+//         await db.client.close();
+
+//         if ( result.deletedCount > 0 ) {
+//             return {
+//                 _id: args._id
+//             };
+//         }
+//     }
+// };
+
+// export default tickets;
