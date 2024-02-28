ecadb72e65c686af398c13e3432272bae67c0c35
hafsaChy <117537827+hafsaChy@users.noreply.github.com>
2024-02-28 17:31:31 +0100

    Update tickets.js

--- backend/test/models/tickets.js
+++ backend/test/models/tickets.js
@@ -1,3 +1,136 @@
+/**
+ * Test opening and resetting the database
+ */
+
+import { describe, it, before, after } from 'mocha';
+import chai from 'chai';
+import sinon from 'sinon';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import database from '../../db/database.js';
+import resetCollection from '../../db/setup.js';
+const { expect, should } = chai;
+
+// Enable 'should' style
+should();
+// test database functions
+describe('Test model', () => {
+    let mongoServer;
+    const colName = 'tickets';
+
+    before(async () => {
+        // Start an in-memory MongoDB server
+        mongoServer = await MongoMemoryServer.create();
+        // Set environment variables
+        process.env.ATLAS_USERNAME = process.env.ATLAS_USERNAME;
+        process.env.ATLAS_PASSWORD = process.env.ATLAS_PASSWORD;
+        process.env.NODE_ENV = 'test';
+        const db = await database.openDb();
+
+        try {
+            const col = await db.collection(colName);
+
+            await col.deleteMany(); // This deletes the data in the collection
+        } catch (err) {
+            console.log("During setup following error occured:", err);
+        } finally {
+            await db.client.close();
+        }
+
+    });
+
+    after(async () => {
+        // Stop the in-memory MongoDB server
+        await mongoServer.stop();
+    });
+
+    /**
+     * Test the model ticketssetup
+     */
+    describe('tickets', () => {
+        let ticketId = null;
+        // Get tickets, should be empty
+        it('should return empty array', async () => {
+            const res = await ticketsModel.getTickets();
+
+            res.should.be.a('array');
+            res.should.have.lengthOf(0);
+        });
+
+        it('should data and array with 1 ticket', async () => {
+            const inData = {
+                code: "ANAtest03",
+                trainnumber: "13579",
+                traindate: "1984-01-01"
+            };
+            const res = await ticketsModel.createTicket(inData);
+
+            res.should.be.a('object');
+            res.should.have.property('_id');
+            res.code.should.equal('ANAtest03');
+
+            // Set ticket id to use in other test
+            ticketId = res._id;
+        });
+
+        it('should return array with one item', async () => {
+            const res = await ticketsModel.getTickets();
+
+            res.should.be.a('array');
+            res.should.have.lengthOf(1);
+            res[0].should.have.property('_id');
+            res[0].trainnumber.should.equal('13579');
+        });
+
+        it('should throw TypeError', async () => {
+            let error;
+
+            try {
+                await ticketsModel.createTicket();
+            } catch (e) {
+                error = e;
+            }
+
+            error.should.be.an.instanceOf(TypeError);
+        });
+
+        it('should change code on ticket', async () => {
+            const args = {
+                _id: ticketId,
+                code: "ANAedit020"
+            }
+            
+            let res = await ticketsModel.updateTicket(args)
+
+            res.should.be.a('object');
+            res.should.have.property('_id');
+            res.code.should.equal('ANAedit020');
+
+            res = await ticketsModel.getTickets();
+            res[0].code.should.equal('ANAedit020');
+        });
+
+        it('delete should empty collection', async () => {
+            const args = {
+                _id: ticketId,
+            }
+            
+            let res = await ticketsModel.deleteTicket(args)
+
+            res.should.be.a('object');
+            res.should.have.property('_id');
+            res._id.should.equal(ticketId);
+
+            res = await ticketsModel.getTickets();
+            res.should.be.a('array');
+            res.should.have.lengthOf(0);
+        });
+    });
+});
+
+
+
+
+
 // // /**
 // //  * Test opening and resetting the database
 // //  */
